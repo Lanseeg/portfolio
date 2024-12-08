a86e897a7b4884429d65ea6914c9eff7e1537e6f
Lanseeg <38041866+Lanseeg@users.noreply.github.com>
2024-12-08 23:13:01 +0100

    feat. styled & filled footer. Skill images reworked

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,20 @@
+// src/components/Footer.jsx
 import '../styles/components/_footer.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+
 const Footer = () => {
   return (
     <footer className="footer">
-      <p> Pierre Gourves - Peran.dev - All rights reserved.</p>
+      <p>Pierre Gourves - Peran.dev - All rights reserved.</p>
+      <div className="footer__icons">
+        <a href="https://www.linkedin.com/in/pierre-gourves/" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} className="footer__icon" />
+        </a>
+        <a href="https://github.com/Lanseeg" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faGithub} className="footer__icon" />
+        </a>
+      </div>
     </footer>
   );
 };
