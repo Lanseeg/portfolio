a56c95e8f753f15f96a6f1f67b3c7b454bc0c5c7
Lanseeg <38041866+Lanseeg@users.noreply.github.com>
2024-12-16 22:02:54 +0100

    fix. Added dotenv to secure ContactForm emailJS

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,36 +1,36 @@
-import { useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import emailjs from 'emailjs-com';
-import '../styles/components/_contactForm.scss';
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import emailjs from "emailjs-com";
+import "../styles/components/_contactForm.scss";
 
 const ContactForm = () => {
-  const { t } = useTranslation('contact');
+  const { t } = useTranslation("contact");
 
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
+    name: "",
+    email: "",
+    message: "",
   });
 
   const [errors, setErrors] = useState({});
-  const [status, setStatus] = useState('');
+  const [status, setStatus] = useState("");
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
-    setErrors({ ...errors, [name]: '' });
-    setStatus('');
+    setErrors({ ...errors, [name]: "" });
+    setStatus("");
   };
 
   const validateForm = () => {
     const newErrors = {};
-    if (!formData.name.trim()) newErrors.name = 'All fields required';
+    if (!formData.name.trim()) newErrors.name = "All fields required";
     if (!formData.email.trim()) {
-      newErrors.email = 'All fields required';
+      newErrors.email = "All fields required";
     } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
-      newErrors.email = 'Check email format';
+      newErrors.email = "Check email format";
     }
-    if (!formData.message.trim()) newErrors.message = 'All fields required';
+    if (!formData.message.trim()) newErrors.message = "All fields required";
 
     setErrors(newErrors);
     return Object.keys(newErrors).length === 0;
@@ -40,23 +40,23 @@ const ContactForm = () => {
     e.preventDefault();
 
     if (!validateForm()) {
-      setStatus('info'); // Status set to 'info' if form is invalid
+      setStatus("info"); // Status set to 'info' if form is invalid
       return;
     }
 
-    const serviceID = 'service_3gqn4om';
-    const templateID = 'template_vkmcdcn';
-    const publicKey = '4uvcdvs7_83AcevC4';
+    const serviceID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+    const templateID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+    const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 
     emailjs
       .send(serviceID, templateID, formData, publicKey)
       .then(() => {
-        setStatus('success');
-        setFormData({ name: '', email: '', message: '' });
+        setStatus("success");
+        setFormData({ name: "", email: "", message: "" });
         setErrors({});
       })
       .catch(() => {
-        setStatus('error');
+        setStatus("error");
       });
   };
 
@@ -71,62 +71,64 @@ const ContactForm = () => {
 
   return (
     <div className="contact-form">
-      <h2>{t('title')}</h2>
+      <h2>{t("title")}</h2>
       <form onSubmit={handleSubmit}>
         <div className="form-group">
-          <label htmlFor="name">{t('name')}</label>
+          <label htmlFor="name">{t("name")}</label>
           <input
             type="text"
             id="name"
             name="name"
-            placeholder={t('name')}
+            placeholder={t("name")}
             value={formData.name}
             onChange={handleChange}
-            className={errors.name ? 'input-error' : ''}
+            className={errors.name ? "input-error" : ""}
           />
         </div>
         <div className="form-group">
-          <label htmlFor="email">{t('email')}</label>
+          <label htmlFor="email">{t("email")}</label>
           <input
             type="email"
             id="email"
             name="email"
-            placeholder={t('email')}
+            placeholder={t("email")}
             value={formData.email}
             onChange={handleChange}
-            className={errors.email ? 'input-error' : ''}
+            className={errors.email ? "input-error" : ""}
           />
         </div>
         <div className="form-group">
-          <label htmlFor="message">{t('message')}</label>
+          <label htmlFor="message">{t("message")}</label>
           <textarea
             id="message"
             name="message"
-            placeholder={t('message')}
+            placeholder={t("message")}
             value={formData.message}
             onChange={handleChange}
-            className={errors.message ? 'input-error' : ''}
+            className={errors.message ? "input-error" : ""}
           />
         </div>
         <button
           type="submit"
-          className={`contact-form__submit ${!isFormValid() ? 'btn-disabled' : ''}`}
+          className={`contact-form__submit ${
+            !isFormValid() ? "btn-disabled" : ""
+          }`}
         >
-          {t('sendButton')}
+          {t("sendButton")}
         </button>
       </form>
 
       {/* Display info message */}
-      {status === 'info' && (
-        <p className="contact-form__status info">{t('infoMessage')}</p>
+      {status === "info" && (
+        <p className="contact-form__status info">{t("infoMessage")}</p>
       )}
 
       {/* Display success or error message */}
-      {status === 'success' && (
-        <p className="contact-form__status success">{t('successMessage')}</p>
+      {status === "success" && (
+        <p className="contact-form__status success">{t("successMessage")}</p>
       )}
-      {status === 'error' && (
-        <p className="contact-form__status error">{t('errorMessage')}</p>
+      {status === "error" && (
+        <p className="contact-form__status error">{t("errorMessage")}</p>
       )}
     </div>
   );
